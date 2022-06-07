45913b908ea5d303943e7d1215c8af3b42dfb2f3
seyedmohamadmoosavi <s.moh.moosavi@gmail.com>
2022-06-07 11:04:24 +0200

    predictions on only one materials

--- src/cp_app/predictions.py
+++ src/cp_app/predictions.py
@@ -2,6 +2,8 @@
 
 """The methods to load ML models and predict the heat capacity using a set of ML features."""
 
+from ast import Raise
+from msilib.schema import Error
 import numpy as np
 import sys
 import pandas as pd
@@ -12,17 +14,42 @@ import copy
 
 FEATURES = cv_features
 
-def predict_Cv_ensemble_structure(ensemble_models: list, FEATURES: list, df_features: pd.DataFrame, structure_name: str) -> list:
+def predict_Cv_ensemble_structure(models: list, FEATURES: list, df_features: pd.DataFrame, temperature: float) -> list:
     """Predict heat capacity using an ensemble of ML models for one structure.
 
-    :param ensemble_models: ensemble of ML models
+    :param models: ensemble of ML models
     :param FEATURES: features for ML model
     :param df_features: pandas dataframe containing the features
-    :param structure_name: the name of structure
+    :param temperature: target temperature 
 
     Returns a list containing the gravimetric and molar heat capacity together with the uncertainty of the models
     """
-    df_site_structure = df_features.loc[df_features["structure_name"]==structure_name]
+    if len(df_features["structure_name"].unique())>2:
+        raise ValueError("More than one structure in the features file...")
+        
+    df_site_structure = copy.deepcopy(df_features)
+    structure_name = df_site_structure["structure_name"].unique()[0]
+    for model_idx,model in enumerate(models):
+        df_site_structure["pCv_{}_predicted_{}".format(temperature, model_idx)]=model.predict(df_site_structure[FEATURES])
+    results=[]
+    predicted_mol=[]
+    predicted_gr=[]
+    for model_idx in range(len(models)):
+        sites=df_site_structure.loc[df_site_structure["structure_name"]==structure_name]
+        predicted_mol.append(np.sum(sites["pCv_{}_predicted_{}".format(temperature,model_idx)])/len(sites))
+        predicted_gr.append(np.sum(sites["pCv_{}_predicted_{}".format(temperature,model_idx)])/np.sum(sites["site AtomicWeight"]))
+    results.append({
+        "name": structure_name,
+        "Cv_gravimetric_{}_mean".format(temperature): np.mean(predicted_gr),
+        "Cv_gravimetric_{}_std".format(temperature): np.std(predicted_gr),
+        "Cv_molar_{}_mean".format(temperature): np.mean(predicted_mol),
+        "Cv_molar_{}_std".format(temperature): np.std(predicted_mol),
+    })
+    return results
+
+
+
+
     predictions_gravimetric = []
     predictions_molar = []
     for model_idx, model in enumerate(ensemble_models):
@@ -45,8 +72,6 @@ def predict_Cv_ensemble_structure(ensemble_models: list, FEATURES: list, df_feat
         
         
     return gr_mean, gr_std, mol_mean, mol_std
-
-
 
 
 def predict_Cv_ensemble_dataset(models: list, FEATURES: list, df_features: pd.DataFrame, temperature: float) -> list:
@@ -104,6 +129,39 @@ def predict_Cv_ensemble_dataset_multitemperatures(path_to_models: str, features_
             all_results=res
         else:
             res= pd.DataFrame(predict_Cv_ensemble_dataset(models, FEATURES,df_features, temperature))
+            all_results=all_results.merge(res, how="inner",on="name")
+
+    if save_to:
+        all_results.to_csv(save_to)
+    return all_results
+
+
+def predict_Cv_ensemble_structure_multitemperatures(path_to_models: str, structure_name: str, features_file: str="features.csv", 
+                FEATURES: list=cv_features, temperatures: list=[300.00], save_to: str="cv_predicted.csv") -> pd.DataFrame:
+    """Predict heat capacity for multiple temperatures using an ensemble of ML models for a dataset.
+
+    :param path_to_models: directory storing the ML models
+    :param structure_name: name of the structure
+    :param FEATURES: features for ML model
+    :param df_features: pandas dataframe containing the features
+    :param temperature: target temperature 
+
+    Returns a list containing the gravimetric and molar heat capacity together with the uncertainty of the models
+    """
+    df_features = pd.read_csv(features_file)
+    df_features["structure_name"]=["_".join(n.split("_")[:-1]) for n in df_features["Unnamed: 0"]]
+    df_features=df_features.loc[df_features["structure_name"]==structure_name]
+    for i,temperature in enumerate(temperatures):
+        models=[]
+        print("loading models for:", temperature)
+        modelnames = glob.glob("{}/{:.2f}/*".format(path_to_models, temperature))
+        models = [joblib.load(n) for n in modelnames]
+        print("{} models loaded, predicting...".format(len(models)))
+        if i==0:
+            res= pd.DataFrame(predict_Cv_ensemble_structure(models, FEATURES, df_features, temperature))
+            all_results=res
+        else:
+            res= pd.DataFrame(predict_Cv_ensemble_structure(models, FEATURES, df_features, temperature))
             all_results=all_results.merge(res, how="inner",on="name")
 
     if save_to:
